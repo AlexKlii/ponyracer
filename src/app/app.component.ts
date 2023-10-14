7ad6011a41cb7f70ea5e7dd2cacd17cf19e719d6
Beater420 <caliaro.alexandre@gmail.com>
2023-10-14 16:35:55 +0200

    3 - List of races
    - Implement Races Component
    - Update e2e and unit test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
+import { RacesComponent } from './races/races.component';
 
 @Component({
   selector: 'pr-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [CommonModule, MenuComponent]
+  imports: [CommonModule, MenuComponent, RacesComponent]
 })
 export class AppComponent {
   title = 'ponyracer';
