6c974dbb13e12346ae23fdb661cbbcbe29e549b4
Beater420 <caliaro.alexandre@gmail.com>
2023-10-16 16:45:20 +0200

    27 - Observable tips and tricks
    - Update `LiveComponent` to implement race status
    - Update `live` method from `RaceService`
    - Update `RaceModel`
    - Update e2e and unit test

--- src/app/live/live.component.ts
+++ src/app/live/live.component.ts
@@ -6,27 +6,48 @@ import { RaceModel } from '../models/race.model';
 import { PonyWithPositionModel } from '../models/pony.model';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { PonyComponent } from '../pony/pony.component';
+import { filter, switchMap, tap } from 'rxjs';
+import { FINISHED_RACE_STATUS, RUNNING_RACE_STATUS } from 'src/constants/status';
+import { FromNowPipe } from '../from-now.pipe';
 
 @Component({
   selector: 'pr-live',
   standalone: true,
-  imports: [CommonModule, PonyComponent],
+  imports: [CommonModule, PonyComponent, FromNowPipe],
   templateUrl: './live.component.html',
   styleUrls: ['./live.component.css']
 })
 export class LiveComponent {
   raceModel: RaceModel | null = null;
   poniesWithPosition: Array<PonyWithPositionModel> = [];
+  winners: Array<PonyWithPositionModel> = [];
+  error = false;
+  betWon: boolean | null = null;
 
   constructor(
     private raceService: RaceService,
     route: ActivatedRoute
   ) {
-    const id = Number(route.snapshot.paramMap.get('raceId')!);
-    this.raceService.get(id).subscribe(race => (this.raceModel = race));
+    const id = +route.snapshot.paramMap.get('raceId')!;
     this.raceService
-      .live(id)
-      .pipe(takeUntilDestroyed())
-      .subscribe(ponies => (this.poniesWithPosition = ponies));
+      .get(id)
+      .pipe(
+        tap((race: RaceModel) => (this.raceModel = race)),
+        filter(race => race.status !== FINISHED_RACE_STATUS),
+        switchMap(() => this.raceService.live(id)),
+        takeUntilDestroyed()
+      )
+      .subscribe({
+        next: ponies => {
+          this.poniesWithPosition = ponies;
+          this.raceModel!.status = RUNNING_RACE_STATUS;
+        },
+        error: e => (this.error = true),
+        complete: () => {
+          this.raceModel!.status = FINISHED_RACE_STATUS;
+          this.winners = this.poniesWithPosition.filter(pony => pony.position >= 100);
+          this.betWon = this.winners.some(pony => this.raceModel!.betPonyId === pony.id);
+        }
+      });
   }
 }
--- src/app/race.service.ts
+++ src/app/race.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { LiveRaceModel, RaceModel } from './models/race.model';
-import { Observable, map } from 'rxjs';
+import { Observable, map, takeWhile } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
-import { PENDING_RACE_STATUS } from '../constants/status';
+import { FINISHED_RACE_STATUS, PENDING_RACE_STATUS } from '../constants/status';
 import { PonyWithPositionModel } from './models/pony.model';
 import { WsService } from './ws.service';
 
@@ -33,6 +33,9 @@ export class RaceService {
   }
 
   live(raceId: number): Observable<Array<PonyWithPositionModel>> {
-    return this.wsService.connect<LiveRaceModel>(`/race/${raceId}`).pipe(map(liveRace => liveRace.ponies));
+    return this.wsService.connect<LiveRaceModel>(`/race/${raceId}`).pipe(
+      takeWhile(liveRace => liveRace.status !== FINISHED_RACE_STATUS),
+      map(liveRace => liveRace.ponies)
+    );
   }
 }
